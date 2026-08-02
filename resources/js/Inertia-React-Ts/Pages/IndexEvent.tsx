d493df045d407650333878696694d9d5d8a3d994
rizkychandra <rizkychandra2204@gmail.com>
2026-08-02 22:46:00 +0700

    refactor: remove unused Badge import from IndexEvent component

--- resources/js/Inertia-React-Ts/Pages/IndexEvent.tsx
+++ resources/js/Inertia-React-Ts/Pages/IndexEvent.tsx
@@ -23,7 +23,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import {
     Table,
     TableBody,
