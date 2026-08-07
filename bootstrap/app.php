f522a79af13d965ae800505d9dcd4582696bbce4
rizkychandra <rizkychandra2204@gmail.com>
2026-08-07 14:49:25 +0700

    feat: implement SecurityHeaders middleware and register it globally in bootstrap app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
         $middleware->alias([
             'access' =>\App\Http\Middleware\Access::class,
         ]);
